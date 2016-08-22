b00b6822ba0bd88284064e5e79f7bc4ce8f5e782
vagrant <vagrant@homestead>
2016-08-22 22:35:44 +0000

    try using the getenv fucntion

--- resources/views/form/stand-small.blade.php
+++ resources/views/form/stand-small.blade.php
@@ -10,6 +10,6 @@
     <label class="col-md-2 control-label">Price</label>
 
     <div class="col-md-6">
-        <input type="text" class="form-control" name="stands[small][{{$i}}][price]" value="{{ env('STAND_SMALL_DEFAULT_PRICE') }}">
+        <input type="text" class="form-control" name="stands[small][{{$i}}][price]" value="{{ getenv('STAND_SMALL_DEFAULT_PRICE') }}">
     </div>
 </div>
