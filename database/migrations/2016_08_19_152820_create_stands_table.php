de42ad9218237925a4dd22aff38da2ed86f1ccfc
vagrant <vagrant@homestead>
2016-08-23 04:04:17 +0000

    check point before the map integration

--- database/migrations/2016_08_19_152820_create_stands_table.php
+++ database/migrations/2016_08_19_152820_create_stands_table.php
@@ -19,8 +19,8 @@ class CreateStandsTable extends Migration
             $table->double('price', 15, 4);
             $table->integer('event_id')->unsigned();
             $table->integer('user_id')->unsigned();
-            $table->integer('totalVisit')->unsigned();
-            $table->integer('totalDownload')->unsigned();
+            $table->integer('totalVisit')->unsigned()->default(0);
+            $table->integer('totalDownload')->unsigned()->default(0);
             $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
             $table->timestamps();
         });
